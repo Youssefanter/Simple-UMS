b2454b70d6dff0b8ced9a11dac983d2d29d8f97b
Youssefanter <yooiialy@gmail.com>
2021-04-01 20:29:37 +0200

    Final commit

---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+$("#add_user").submit(function (event) {
+  alert("Data Inserted succefully!");
+});
+
+$("#update_user").submit(function (event) {
+  event.preventDefault();
+  var unindexed_array = $(this).serializeArray();
+  var data = {};
+  $.map(unindexed_array, function (n, i) {
+    data[n["name"]] = n["value"];
+  });
+  console.log(data);
+  var request = {
+    url: `http://localhost:3000/api/users/${data._id}`,
+    method: "PUT",
+    data: data,
+  };
+  console.log("anything");
+  $.ajax(request).done(function (response) {
+    alert("Data Updated succefully!");
+  });
+});
+
+if(window.location.pathname == "/"){
+  $ondelete = $(".table tbody td a.delete");
+  $ondelete.click(function(){
+      var id = $(this).attr("data-id")
+
+      var request = {
+          "url" : `http://localhost:3000/api/users/${id}`,
+          "method" : "DELETE"
+      }
+
+      if(confirm("Do you really want to delete this record?")){
+          $.ajax(request).done(function(response){
+              alert("Data Deleted Successfully!");
+              location.reload();
+          })
+      }
+
+  })
+}
--- server/controller/controller.js
+++ server/controller/controller.js
@@ -19,7 +19,8 @@ exports.create = (req, res) => {
   user
     .save(user)
     .then((data) => {
-      res.send(data);
+      //res.send(data);
+      res.redirect("/add-user");
     })
     .catch((err) => {
       res.status(500).send({
@@ -29,10 +30,78 @@ exports.create = (req, res) => {
 };
 
 //retrive and get all users
-exports.find = (req, res) => {};
+exports.find = (req, res) => {
+  if (req.query.id) {
+    const id = req.query.id;
+    userdb
+      .findById(id)
+      .then((data) => {
+        if (!data) {
+          res.status(404).send({ Messsage: "Not found data with id=" + id });
+        } else {
+          res.send(data);
+        }
+      })
+      .catch((err) => {
+        res.status(500).send({ message: "Error retriving user id=" + id });
+      });
+  } else {
+    userdb
+      .find()
+      .then((user) => {
+        res.send(user);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message:
+            err.message || "Error occuerd while retriving user information",
+        });
+      });
+  }
+};
 
 //update a new identified user
-exports.update = (req, res) => {};
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({ message: "Data to update can not be empty" });
+  }
+
+  const id = req.params.id;
+  userdb
+    .findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot Update user with ${id}. Maybe user not found!`,
+        });
+      } else {
+        res.send(data);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "Error Update user information" });
+    });
+};
 
 //delete a user with specified id
-exports.delete = (req, res) => {};
+exports.delete = (req, res) => {
+  const id = req.params.id;
+  userdb
+    .findByIdAndDelete(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `cannot delete with id: ${id}. Maybe user not found!`,
+        });
+      } else {
+        res.send({
+          message: "User was deleted succenfully",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete User with id=" + id,
+      });
+    });
+};
